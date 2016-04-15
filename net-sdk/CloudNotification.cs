1ed64b18df87ea516177ecccc469c5070d92507b
Daryl Cumbo <daryl.cumbo@betssongroup.com>
2016-04-15 09:45:55 +0200

    Adds channelName to chanel in CloudNotification

--- net-sdk/CloudNotification.cs
+++ net-sdk/CloudNotification.cs
@@ -1,52 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Quobject.SocketIoClientDotNet.Client;
-
 
 namespace CB
 {
     public class CloudNotification
     {
-
-        private static Socket _socket = IO.Socket(CloudApp.ApiUrl);
+        private static readonly Socket _socket = IO.Socket(CloudApp.ApiUrl);
 
         public static void On(string channelName, Callback callback)
         {
-            CB.PrivateMethods.Validate();
+            PrivateMethods.Validate();
             _socket.Emit("join-custom-channel", CloudApp.AppID + channelName);
-            _socket.On(CloudApp.AppID + channelName, (data) =>
-            {
-                callback(data);
-            });
+            _socket.On(CloudApp.AppID + channelName, data => { callback(data); });
         }
 
         public static void Off(string channelName)
         {
-            CB.PrivateMethods.Validate();
+            PrivateMethods.Validate();
             _socket.Emit("leave-custom-channel", CloudApp.AppID + channelName);
-            _socket.Off(CB.CloudApp.AppID + channelName);
+            _socket.Off(CloudApp.AppID + channelName);
         }
 
-        public static void Publsh(string channelName, Object data)
+        public static void Publsh(string channelName, object data)
         {
-            CB.PrivateMethods.Validate();
-            Dictionary<string, object> jsonObj = new Dictionary<string, object>();
-            jsonObj.Add("channel", CB.CloudApp.AppID);
-            jsonObj.Add("data", data);
+            PrivateMethods.Validate();
+            var jsonObj = new Dictionary<string, object>
+            {
+                { "channel", CloudApp.AppID + channelName },
+                { "data", data }
+            };
             _socket.Emit("publish-custom-channel", jsonObj);
         }
-
     }
 }
-
-
-
-
-
-
-
-
-
